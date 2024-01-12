e0a3c9a557bc09c739e4b7c76ab49f7a62504421
flom <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>
2024-01-12 19:38:59 +0100

    specialized components for the rows

---
+++ webapp/src/api/groups.hooks.ts
@@ -0,0 +1,31 @@
+import {
+  useMutation,
+  type UseMutationResult,
+  useQuery,
+  useQueryClient,
+  type UseQueryResult,
+} from "@tanstack/react-query";
+import type { Group } from "@/models/Group";
+import { getGroups } from "@/api/queries/getGroups";
+import { createGroup, type CreateGroupArgs } from "@/api/mutations/createGroup";
+
+function useGroups(): UseQueryResult<Group[]> {
+  return useQuery(getGroups());
+}
+
+function useCreateGroup(): UseMutationResult<
+  unknown,
+  unknown,
+  CreateGroupArgs
+> {
+  const queryClient = useQueryClient();
+
+  return useMutation(createGroup(queryClient));
+}
+
+const groupsHooks = {
+  useGroups,
+  useCreateGroup,
+};
+
+export default groupsHooks;
