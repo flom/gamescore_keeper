f03837b67dc1ee4e74e60d68ed042c76da9e63b2
flom <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>
2024-03-12 07:26:43 +0100

    package for pocketbase

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -66,4 +66,12 @@ export default defineConfig(({ mode }) => ({
       ),
     },
   },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:8090",
+        changeOrigin: true,
+      },
+    },
+  },
 }));
